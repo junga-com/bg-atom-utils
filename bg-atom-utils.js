22c2f41ceee133bb616509b286a3944fd19373aa
Robert E. Griffith <36312888+bobjunga@users.noreply.github.com>
2020-03-02 20:01:05 -0500

    added BGTimer, BGFeedbackView, and README.md

--- bg-atom-utils.js
+++ bg-atom-utils.js
@@ -2,8 +2,84 @@
 import { el, list, mount, setAttr } from 'redom';
 import { Disposable, CompositeDisposable } from 'atom';
 
+export class BGTimer {
+	constructor(prec) {
+		this.prec = (prec!==undefined) ? prec : 3;
+		this.startTime = null;
+		this.lap   = null;
+		this.ticks = [];
+		this.tickAccumulation = 0.0;
+		this.start();
+	}
 
-const defaultOptions = {
+	start() {
+		var current = process.hrtime.bigint();
+		this.startTime = current;
+		this.lap   = current;
+		this.ticks = [];
+		this.ticks.push({
+			startTime: current,
+			endTime: null,
+			delta: null
+		})
+	}
+
+	// call at the top of callbacks and after awaits
+	tickStart() {
+		this.ticks.push({
+			startTime: process.hrtime.bigint(),
+			endTime: null,
+			delta: null
+		})
+	}
+	// call at the end of callbacks and before awaits (just before they yield)
+	tickEnd() {
+		var tick = this.ticks[this.ticks.length-1];
+		if (tick.endTime)
+			console.log("BGTimer: error: tickEnd called without a matching tickStart/start");
+		else {
+			tick.endTime = process.hrtime.bigint();
+			tick.delta = this.bgNanoToSec(tick.endTime-tick.startTime);
+			this.tickAccumulation += tick.delta;
+		}
+	}
+
+	get()   {return this.bgNanoToSec(process.hrtime.bigint() - this.startTime) }
+	print(description, prec) {
+		var prec = (prec!==undefined) ? prec : this.prec;
+		console.log('total : '+this.get().toFixed(prec)+' : '+description);
+	}
+
+	lapGet()   {return this.bgNanoToSec(process.hrtime.bigint() - this.lap) }
+	lapPrint(description, prec) {
+		var prec = (prec!==undefined) ? prec : this.prec;
+		var current = process.hrtime.bigint();
+		var delta = this.bgNanoToSec(current - this.lap);
+		this.lap = current;
+		console.log('lap : '+delta.toFixed(prec)+' : '+description);
+	}
+
+	printDetails(description) {
+		this.tickEnd();
+		var total = this.get()
+		var activePercent = this.tickAccumulation * 100.0 / total;
+		console.log("BGTimer ended:"+description);
+		console.log("   real elapsed  : "+total.toFixed(3));
+		console.log("   active elapsed: "+this.tickAccumulation.toFixed(3));
+		console.log("   % active      : "+activePercent.toFixed(0)+'%');
+	}
+
+	bgNanoToSec(nano) {
+		var nano = nano.toString();
+		while (nano.length < 10) nano = '0'+nano;
+		var frac=nano.slice(-9);
+		var sec=nano.slice(0, nano.length-9);
+		return parseFloat(sec+'.'+frac);
+	}
+}
+
+
+const defaultBGAtomViewOptions = {
 	title:           'BG View',
 	defaultLocation: 'bottom',
 	allowedLocations: ['center', 'left', 'right', 'bottom'],
@@ -11,12 +87,15 @@ const defaultOptions = {
 } 
 
 
+
 // This is a generic component for creating new Atom items dislayed in panes. This class can be used as is to open an empty view.
 // Typically, you would make a new class that extends this, register a URI opener in atom.workspace and return a new instance 
 // of that class from the opener.
 // Features:
 //    onDomReady() : overridable method. This method gets called after the view is added to the DOM and is located in a pane or panel
-//    onResize()   : overridable method. This gets called when you view changes size
+//    onResize()   : overridable method. This gets called when the view changes size
+//    onFocus()    : overridable method. This gets called when the view receives the focus. Often you need to delegate focus to
+//                   a specific child component of the view 
 //    this.options : gets passed through constructors to implement defaults at each class in the hiearchy to set Atom features
 // How To Use:
 // In your plugin entry file (see package.json:main) activate() method, register an opener callback which returns a new instance
@@ -32,7 +111,7 @@ export class BGAtomView {
 	constructor(uri, parent, options) {
 		this.uri     = uri;
 		this.parent  = parent;
-		this.options = Object.assign({}, defaultOptions, options) || {};
+		this.options = Object.assign({}, defaultBGAtomViewOptions, options) || {};
 		this.subscriptions = new CompositeDisposable();
 
 		// Create root element
@@ -50,7 +129,8 @@ export class BGAtomView {
 
 	// this is the internal onDomReady callback gernerated by the iframe trick 
 	_postCtor(ev) {
-		if (this.tFrame.contentWindow != ev.source) return;
+		console.log("_postCtor(ev):"+require('util').inspect(ev, { depth: 2 }));
+		if (!this.tFrame || this.tFrame.contentWindow != ev.source) return;
 		this.tFrame.remove(); delete this.tFrame;
 		let pane = atom.workspace.paneForURI(this.uri);
 		console.log(require('util').inspect([this.uri, pane], { depth: null }));
@@ -59,6 +139,15 @@ export class BGAtomView {
 			this.subscriptions.add(pane.onDidChangeActiveItem((e) => {this.onResize(e)}));
 		}
 		this.addWinListener('resize', (ev) => this.onResize(ev));
+
+		// Add callbacks to run for current and future active items on active panes.
+		this.subscriptions.add(atom.workspace.observeActivePaneItem((item) => {
+			// In callback, focus specifically on terminal when item is terminal item.
+			if (item instanceof BGAtomView) {
+				this.onFocus()
+			}
+		}))
+
 		this.onDomReady()
 	}
 
@@ -68,6 +157,9 @@ export class BGAtomView {
 	// override this to perform actions when the view changes size
 	onResize(e) {console.log('base class onResize');}
 
+	// override this to perform actions when the view receives focus
+	onFocus(e)   {console.log('base class onFocus');}
+
 	getTitle()            {return this.options['title'] || 'BG Tool';}
 	getElement()          {return this.rootElement;}
 	getDefaultLocation()  {return this.options['defaultLocation'] || 'bottom';}
@@ -75,6 +167,13 @@ export class BGAtomView {
 	getURI()              {return this.uri;}
 	isPermanentDockItem() {return this.options['isPermanent'] || false}
 
+	show() {
+		atom.workspace.open(this.uri)
+	}
+
+	toggle() {
+		atom.workspace.toggle(this.uri)
+	}
 
 	destroy() {
 		this.subscriptions.dispose();
@@ -85,4 +184,29 @@ export class BGAtomView {
 		window.addEventListener(eventName, callback);
 		this.subscriptions.add(new Disposable(() => window.removeEventListener(eventName, callback)));
 	}
+}
+
+
+export class BGFeedbackView {
+	constructor() {
+		// Create root element
+		this.rootElement = el('div.atom-cyto-message', "here, baby");
+		mount(document.body, this.rootElement);
+
+		this.modalPanel = atom.workspace.addModalPanel({
+			item: this.rootElement,
+			visible: true
+		});
+	}
+
+	setMessage(data) {
+		this.rootElement.textContent = data;
+	}
+
+	isVisible() {this.modalPanel.isVisible()}
+	show()      {this.modalPanel.show()}
+	hide()      {this.modalPanel.hide()}
+	destroy()   {this.modalPanel.destroy();}
+
+	serialize() {}
 }
